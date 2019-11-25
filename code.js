8192354bb1f95e1b49afab185d03e72b4cb9fafe
Tom Ruff <ruffmantom@gmail.com>
2019-11-25 14:13:25 -0700

    added styles and some js getting there

--- code.js
+++ code.js
@@ -1,7 +1,34 @@
 console.log('working');
 
 
+// Checking for character range is set and between 8-25 charaters
 
-function clickedButton(button) {
+
+function myFunction() {
+    var charRange = document.getElementById("myNumber").value;
+    if (charRange <= 25 && charRange >= 8) {
+        console.log('great!');
+    } else {
+        alert('Please choose a number between 8-25');
+    }
+}
+
+function getUpperCase() {
 
 }
+function getLowerCase() {
+
+}
+function getSpecialCase() {
+
+}
+function getNum() {
+
+}
+
+//  need to get functions for finding char sets
+
+
+
+
+
