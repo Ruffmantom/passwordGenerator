6c22ea8f7a8fbe48e852123c93201d41805abb3a
Tom Ruff <ruffmantom@gmail.com>
2019-11-26 13:29:06 -0700

    final commit going to add this to my website

--- code.js
+++ code.js
@@ -1,91 +1,32 @@
-console.log('working');
+// after over thinking and working with functions to get random charaters I found an easy way with looping through a string.
+var result = document.getElementById('generateBox');
+let userNum = document.getElementById('myNumber');
+var submitBtn = document.getElementById('submitBtn');
 
+var allChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
 
-// Checking for character range is set and between 8-25 charaters resource for adding input resource w3 schools
-var result = document.getElementById('generateBox')
+submitBtn.addEventListener('click', function (e) {
 
-function generatePass() {
-    let userNum = document.getElementById("myNumber").value;
-    if (userNum <= 25 && userNum >= 8) {
-        console.log('great!');
-    } else {
-        alert('Please choose a number between 8-25');
+    var characters = allChars;
+    yourPass.value = password(userNum.value, characters);
+
+});
+function password(length, characters) {
+    var finPass = '';
+    for (var i = 0; i < length; i++) {
+        if (length <= 25 && length >= 8) {
+            finPass += characters.charAt(Math.floor(Math.random() * characters.length));
+        };
+
     };
-    // Rusults box
-    const length = userNum;
-    console.log(length);
-
-    // adding the results for the password
-    result.innerHTML = generatedPassword(length);
-
+    return finPass;
+    console.log(finPass);
 };
-
-
-// final GENERATION function 
-function generatedPassword(length) {
-
-    var typesArr = [true, true, true, true];
-    let password = '';
-    var typeCount = typesArr;
-
-    for (i = 0; i <= length; i++) {
-        typesArr.forEach(type => {
-            const funcName = Object.keys(type)[0];
-            password += funcHolder[funcName]();
-
-        })
-
-    }
-
-}
-
-// setting types to all true since user cannot uncheck a char type
-var typesArr = [true, true, true, true];
-
-// Added a object to hold all the functions
-var funcHolder = {
-    lower: getLowerCase,
-    upper: getUpperCase,
-    special: getSpecialCase,
-    numbers: getNum
-}
-
-// got functions to get random numbers and letters help from youtube resource "Traversy Media"
-function getLowerCase() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-
-}
-
-function getUpperCase() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getSpecialCase() {
-    return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
-}
-
-function getNum() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
 
 // Function for COPY to clipboard resource w3 schools
 function copyToClipboard() {
-    var copyText = document.getElementById("generateBox");
+    var copyText = document.getElementById("yourPass");
     copyText.select();
-
     document.execCommand("copy");
-    alert(copyText.value);
-}
-
-
-
-
-
-
-
-
-
-
-
-
+    // alert(copyText.value);
+};
